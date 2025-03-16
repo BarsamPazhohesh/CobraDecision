a2e3e2fd9f89fa47f510661edaf9967e37f0142c
rezishon <rezashahmoradi.81@outlook.com>
2025-03-16 11:53:01 +0330

    doc: add functions explanation
    They have
    - Brief explanation
    - Parameters
    - Return values

--- Rezishon/GolangProjs/Repo/CanJump.go
+++ Rezishon/GolangProjs/Repo/CanJump.go
@@ -1,4 +1,15 @@
 package repo
+
+// The main function of CanJump Question
+// Handle main logic.
+//
+// * Parameters:
+//
+//	input ([]int): The array of int that we goes over it.
+//
+// * Returns:
+//
+//	bool: Returns true if we read the end of the input array.
 func CanJump(input []int) bool {
 
 	inputLen := len(input)
@@ -15,6 +26,18 @@ func CanJump(input []int) bool {
 	}
 	return false
 }
+
+// Calculate which step with what value should we work with in each iteration
+//
+// * Parameters:
+//
+//	i (*int): The iteration variable
+//	indexValue (*int): Value of each index of the array
+//	value (int): The value which we will set to "indexValue"
+//
+// * Returns:
+//
+//	No return because it only sets values
 func Cal(i, indexValue *int, value int) {
 	*i += *indexValue
 	*indexValue = value
