0555d57e61b4557f6d3be553529db56582cf7a99
rezishon <rezashahmoradi.81@outlook.com>
2025-03-16 16:16:57 +0330

    feat: iterate over "inputArray"
    By a for loop we will iterate over the "inputArray" and count how many
    times we saw the specific number.

--- Rezishon/GolangProjs/Repo/Remove_Duplicates_from_Sorted_Array/Remove-Duplicates-from-Sorted-Array.go
+++ Rezishon/GolangProjs/Repo/Remove_Duplicates_from_Sorted_Array/Remove-Duplicates-from-Sorted-Array.go
@@ -5,5 +5,9 @@ import "fmt"
 func Remove_Duplicates_from_Sorted_Array(inputArray []int) {
 
 	seenValues := make(map[int]int)
+
+	for i := 0; i < len(inputArray); i++ {
+		seenValues[inputArray[i]] += 1
+	}
 }
 
