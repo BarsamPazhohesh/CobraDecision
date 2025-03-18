21c45153d019b3c8d74a8f83ef0c989e4f4db6cb
Ahora9roze <Ahora496@gmail.com>
2025-03-18 13:03:11 +0330

    jumpGame
    it's done!

---
+++ Shayan/firstRound/jumpGame.py
@@ -0,0 +1,23 @@
+''''''''''''''''''''' JUMP GAME '''''''''''''''''''''
+
+
+nums = []
+userNums = input("Enter the numbers separated by commas: ")
+
+for i in userNums.split(","):
+    nums.append(int(i))
+
+print(f"nums are: {nums}") # debug
+
+def canJump(nums):
+    maxReach = 0
+    for i in range(len(nums)):
+        if i > maxReach:
+            return False
+        maxReach = max(maxReach, i + nums[i])
+        print(f"maxReach is: {maxReach}") # debug
+    return True
+
+    
+
+print(f"can we jump: {canJump(nums)}") # result
