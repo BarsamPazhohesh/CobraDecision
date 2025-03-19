86f1ac4704bd6aa031ce61da2968845900abd219
Rezishon.Pc <rezashahmoradi.81@outlook.com>
2025-03-19 13:54:05 +0330

    feat: add nested loop
    By using for loop inside each other, we will iterate over the nums
    array.

--- Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
+++ Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
@@ -1,3 +1,7 @@
 function twoSum(nums: number[], target: number): number[] {
   let result: number[] = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+    }
+  }
 }
