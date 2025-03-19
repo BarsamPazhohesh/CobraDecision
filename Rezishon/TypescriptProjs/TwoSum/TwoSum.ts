a12ccf7742733e42bcda6610fa3607b43ace7ec3
Rezishon.Pc <rezashahmoradi.81@outlook.com>
2025-03-19 14:02:50 +0330

    feat: add main function
    This function will act like a start point in this question solution. The
    tests also contains.

--- Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
+++ Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
@@ -1,3 +1,10 @@
+function main() {
+  console.log(twoSum([2, 7, 11, 15], 9));
+  console.log(twoSum([3, 2, 4], 6));
+  console.log(twoSum([3, 3], 6));
+}
+
+main();
 function twoSum(nums: number[], target: number): number[] {
   let result: number[] = [];
   for (let i = 0; i < nums.length - 1; i++) {
