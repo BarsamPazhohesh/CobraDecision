31aec1e9d1db988c5097305464e17b0d09b4dddd
Rezishon.Pc <rezashahmoradi.81@outlook.com>
2025-03-19 13:55:38 +0330

    feat: add condition checker
    The "Is_i_j_eq_target" function will help us check the question
    condition. The i, j, target values will pass throw the method in object
    type(an interface).

--- Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
+++ Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
@@ -2,6 +2,8 @@ function twoSum(nums: number[], target: number): number[] {
   let result: number[] = [];
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
+      if (Is_i_j_eq_target({ i: nums[i], j: nums[j], target: target })) {
+      }
     }
   }
 }
