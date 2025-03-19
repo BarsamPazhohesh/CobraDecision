40f619552f29cd4899b8b6aa53295d7154baeb9e
Rezishon.Pc <rezashahmoradi.81@outlook.com>
2025-03-19 13:57:37 +0330

    feat: return the result
    After we added wanted indexes due to the question condition, we will
    return the result array.

--- Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
+++ Rezishon/TypescriptProjs/TwoSum/TwoSum.ts
@@ -3,7 +3,11 @@ function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (Is_i_j_eq_target({ i: nums[i], j: nums[j], target: target })) {
+        result.push(i);
+        result.push(j);
+        return result;
       }
     }
   }
+  return [];
 }
