a28eb16d88ea8e0c389b40e97ce6cd7c5fe9ec2d
rezishon <rezashahmoradi.81@outlook.com>
2025-04-26 00:32:18 +0330

    feat: add second value of array
    The second value will be one.
    If the user wants two result the `zero, & one` in an array will show up
    to him.

--- Rezishon/TypescriptProjs/Fibonacci/src/fibonacci.ts
+++ Rezishon/TypescriptProjs/Fibonacci/src/fibonacci.ts
@@ -4,6 +4,11 @@ function FiboCal(limiter: number): number[] {
     if (limiter == 1) {
       return returnNums;
     }
+
+    returnNums.push(1);
+    if (limiter == 2) {
+      return returnNums;
+    }
   }
   return [];
 }
