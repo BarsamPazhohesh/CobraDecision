24c56f335899905ccda09142a2974819da9d6bb3
rezishon <rezashahmoradi.81@outlook.com>
2025-04-26 00:31:27 +0330

    feat: add first value of array
    The first value will be zero.
    If the user wants only one result the `zero` in an one index array will
    show up to him.

--- Rezishon/TypescriptProjs/Fibonacci/src/fibonacci.ts
+++ Rezishon/TypescriptProjs/Fibonacci/src/fibonacci.ts
@@ -1,5 +1,9 @@
 function FiboCal(limiter: number): number[] {
   if (limiter > 0) {
+    let returnNums: number[] = [0];
+    if (limiter == 1) {
+      return returnNums;
+    }
   }
   return [];
 }
